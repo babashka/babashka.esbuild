49f03f8a1be52ad5b6e45eb878ab6a0bff262d3a
Michiel Borkent <michielborkent@gmail.com>
2026-09-13 18:16:25 +0200

    Read shim sources with babashka.fs

--- bb/shim.clj
+++ bb/shim.clj
@@ -1,10 +1,9 @@
 (ns shim
-  (:import [java.io File]
-           [java.nio.file Files]
-           [java.security MessageDigest]))
+  (:require [babashka.fs :as fs])
+  (:import [java.security MessageDigest]))
 
 (defn source-hash [dir]
   (let [md (MessageDigest/getInstance "SHA-256")]
     (doseq [f ["shim.go" "go.mod" "go.sum"]]
-      (.update md (Files/readAllBytes (.toPath (File. (str dir) f)))))
+      (.update md (fs/read-all-bytes (fs/path dir f))))
     (subs (format "%064x" (BigInteger. 1 (.digest md))) 0 16)))
